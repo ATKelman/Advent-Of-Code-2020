5cf1f0fac44ec37d12cc43cfc6988f4cb2f4ceed
Alexander Kelman <alexanderkelman95@hotmail.com>
2020-12-02 19:21:10 +0100

    Added Day03, created Day3 text file.

---
+++ Advent Of Code 2020/Days/Day03.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advent_Of_Code_2020.Days
+{
+    public class Day03 : DayBase
+    {
+        public Day03(int day)
+            : base(day)
+        {
+
+        }
+
+        public override string SolvePart1()
+        {
+            return "";
+        }
+
+        public override string SolvePart2()
+        {
+            return "";
+        }
+    }
+}
--- Advent Of Code 2020/Program.cs
+++ Advent Of Code 2020/Program.cs
@@ -9,7 +9,7 @@ namespace Advent_Of_Code_2020
         {
             Console.WriteLine("Advent of Code 2020");
 
-            var day = new Day02(2);
+            var day = new Day03(3);
 
             var result = day.SolvePart1();
 
