1bfabb8d0cf64aa7c70f1ea9f52cd89fcdde27ca
Alexander Kelman <alexanderkelman95@hotmail.com>
2020-12-03 09:01:44 +0100

    Added solutions for day 3 part 1 & 2.

--- Advent Of Code 2020/Days/Day03.cs
+++ Advent Of Code 2020/Days/Day03.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace Advent_Of_Code_2020.Days
 {
     public class Day03 : DayBase
     {
+        private string[] _input;
+
         public Day03(int day)
             : base(day)
         {
@@ -14,12 +20,50 @@ namespace Advent_Of_Code_2020.Days
 
         public override string SolvePart1()
         {
-            return "";
+            _input = File.ReadAllLines(_inputPath);
+
+            var treesHit = CalculatePath(3, 1);
+
+            return $"Trees hit: {treesHit}";
         }
 
         public override string SolvePart2()
         {
-            return "";
+            var slopes = new List<Tuple<int, int>>()
+            {
+                new Tuple<int, int>(1, 1),
+                new Tuple<int, int>(3, 1),
+                new Tuple<int, int>(5, 1),
+                new Tuple<int, int>(7, 1),
+                new Tuple<int, int>(1, 2),
+            };
+
+            BigInteger treesHit = 1;
+            foreach (var slope in slopes)
+                treesHit *= CalculatePath(slope.Item1, slope.Item2);
+
+            return $"Trees hit: {treesHit}";
+        }
+
+        private int CalculatePath(int stepsRight, int stepsDown)
+        {
+            var xPos = 0;
+            var yPos = 0;
+
+            bool insideMap = true;
+            int treesHit = 0;
+            while (insideMap)
+            {
+                xPos = ((xPos + stepsRight) >= _input[yPos].Length) ? (xPos + stepsRight) % _input[yPos].Length : xPos + stepsRight;
+                yPos += stepsDown;
+
+                if (yPos >= _input.Count())
+                    insideMap = false;
+                else if (_input[yPos][xPos] == '#')
+                    treesHit++;
+            }
+
+            return treesHit;
         }
     }
 }
