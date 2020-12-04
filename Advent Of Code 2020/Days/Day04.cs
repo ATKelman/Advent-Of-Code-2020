7cd2589f3db47dc8d08dca0e8d72048ae9283fb2
Alexander Kelman <alexanderkelman95@hotmail.com>
2020-12-04 08:56:29 +0100

    Added Day 04 class & text file

---
+++ Advent Of Code 2020/Days/Day04.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Advent_Of_Code_2020.Days
+{
+    public class Day04 : DayBase
+    {
+        public Day04(int day)
+            : base(day)
+        {
+
+        }
+
+        public override string SolvePart1()
+        {
+            var input = File.ReadAllText(_inputPath);
+
+            return "";
+        }
+
+        public override string SolvePart2()
+        {
+            return "";
+        }
+    }
+}
