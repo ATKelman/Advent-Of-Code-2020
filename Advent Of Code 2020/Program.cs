50f9365f58b77893bb4c1f469e579b1eb4a2f291
Alexander Kelman <alexanderkelman95@hotmail.com>
2020-12-05 21:13:18 +0100

    Changed program to run day 5

--- Advent Of Code 2020/Program.cs
+++ Advent Of Code 2020/Program.cs
@@ -9,7 +9,7 @@ namespace Advent_Of_Code_2020
         {
             Console.WriteLine("Advent of Code 2020");
 
-            var day = new Day04(4);
+            var day = new Day05(5);
 
             var result = day.SolvePart1();
 
