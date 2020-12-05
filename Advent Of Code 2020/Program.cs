d413d5e60785ad8c0a2ca6255391381abfdc1f6a
Alexander Kelman <alexanderkelman95@hotmail.com>
2020-12-05 22:01:48 +0100

    Added Day 6 class

--- Advent Of Code 2020/Program.cs
+++ Advent Of Code 2020/Program.cs
@@ -9,7 +9,7 @@ namespace Advent_Of_Code_2020
         {
             Console.WriteLine("Advent of Code 2020");
 
-            var day = new Day05(5);
+            var day = new Day06(6);
 
             var result = day.SolvePart1();
 
