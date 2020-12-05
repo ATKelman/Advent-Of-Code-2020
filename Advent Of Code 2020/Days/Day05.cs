b34fdf2bd7439ec8b3d90dafe87544d074baedba
Alexander Kelman <alexanderkelman95@hotmail.com>
2020-12-05 22:00:09 +0100

    Added Day 05 solutions for part 1 & 2

--- Advent Of Code 2020/Days/Day05.cs
+++ Advent Of Code 2020/Days/Day05.cs
@@ -1,25 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace Advent_Of_Code_2020.Days
 {
     public class Day05 : DayBase
     {
+        private List<int> seatIds = new List<int>();
+
         public Day05(int day)
             : base(day)
         {
-
+            GetSeats();
         }
 
         public override string SolvePart1()
-        {
-            return "";
+        {     
+            return seatIds.Max().ToString();
         }
 
         public override string SolvePart2()
         {
-            return "";
+            var mySeatId = 0;
+            var seat = seatIds.OrderBy(x => x).Aggregate((prev, next) =>
+            {
+                if ((next - prev) == 2)
+                    mySeatId = prev + 1;
+                return next;
+            });
+
+            return mySeatId.ToString();
+        }
+
+        private void GetSeats()
+        {
+            var input = File.ReadAllLines(_inputPath);
+
+            foreach (var seat in input)
+            {
+                var row = Enumerable.Range(0, 128).ToList();
+                var col = Enumerable.Range(0, 8).ToList();
+
+                foreach (var i in seat)
+                {
+                    if (i == 'F')
+                        row = row.Take(row.Count() / 2).ToList();
+                    else if (i == 'B')
+                        row = row.Skip(row.Count() / 2).Take(row.Count() / 2).ToList();
+                    else if (i == 'L')
+                        col = col.Take(col.Count() / 2).ToList();
+                    else if (i == 'R')
+                        col = col.Skip(col.Count() / 2).Take(col.Count() / 2).ToList();
+                }
+
+                var seatId = row[0] * 8 + col[0];
+                seatIds.Add(seatId);
+            }
         }
     }
 }
