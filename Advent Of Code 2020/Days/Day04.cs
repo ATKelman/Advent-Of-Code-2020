0a61a51fb0e98ad6592978d5983ad926241aa588
Alexander Kelman <alexanderkelman95@hotmail.com>
2020-12-05 21:11:57 +0100

    Day 04 cleanup

--- Advent Of Code 2020/Days/Day04.cs
+++ Advent Of Code 2020/Days/Day04.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Advent_Of_Code_2020.Days
 {
@@ -15,14 +17,34 @@ namespace Advent_Of_Code_2020.Days
 
         public override string SolvePart1()
         {
-            var input = File.ReadAllText(_inputPath);
+            var validPassports = 0;
+            File.ReadAllText(_inputPath).Split(new string[] { "\r\n\r\n" }, StringSplitOptions.RemoveEmptyEntries).ToList().ForEach(x =>
+            {
+                var regex = Regex.Matches(x, "eyr|iyr|byr|ecl|pid|hcl|hgt");
+                if (regex.Count() == 7)
+                    validPassports++;
+            });
 
-            return "";
+            return validPassports.ToString();
         }
 
         public override string SolvePart2()
         {
-            return "";
+            var validPassports = 0;
+            File.ReadAllText(_inputPath).Split(new string[] { "\r\n\r\n" }, StringSplitOptions.None).ToList().ForEach(x =>
+            {
+                var regex = Regex.Matches(x, 
+                    "eyr:(202[0-9]|2030)" +
+                    "|iyr:(201[0-9]|2020)" +
+                    "|byr:(19[2-9][0-9]|200[0-2])" +
+                    "|ecl:(amb|blu|brn|gry|grn|hzl|oth)" +
+                    "|pid:[0-9]{9}" +
+                    "|hcl:#[\\w]{6}" +
+                    "|hgt:(1([5-8][0-9]|9[0-3])cm|(59|6[0-9]|7[0-6])in)");
+                if (regex.Count() == 7)
+                    validPassports++;
+            });
+            return validPassports.ToString();
         }
     }
 }
