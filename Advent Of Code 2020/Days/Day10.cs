1e0899d9dd7accf9a2dbd2f8a3b96d8cc0f2bfab
Alexander Kelman <alexanderkelman95@hotmail.com>
2020-12-09 22:28:35 +0100

    Added Day 10

---
+++ Advent Of Code 2020/Days/Day10.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Advent_Of_Code_2020.Days
+{
+    public class Day10 : DayBase
+    {
+        public Day10(int day)
+            : base(day)
+        {
+
+        }
+
+        public override string SolvePart1()
+        {
+            return "";
+        }
+
+        public override string SolvePart2()
+        {
+            return "";
+        }
+    }
+}
