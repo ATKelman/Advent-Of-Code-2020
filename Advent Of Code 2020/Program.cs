f007eb9775bb9d90196d287d54398adc27b4fdd5
Alexander Kelman <alexanderkelman95@hotmail.com>
2020-12-14 20:17:17 +0100

    Added Day 15

--- Advent Of Code 2020/Program.cs
+++ Advent Of Code 2020/Program.cs
@@ -9,7 +9,7 @@ namespace Advent_Of_Code_2020
         {
             Console.WriteLine("Advent of Code 2020");
 
-            var day = new Day14(14);
+            var day = new Day15(15);
 
             var result = day.SolvePart1();
 
