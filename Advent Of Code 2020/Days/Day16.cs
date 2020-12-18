b3d25fdfc41602145211b1a20672a30462f76864
Alexander Kelman <alexanderkelman95@hotmail.com>
2020-12-18 19:44:36 +0100

    Added Day 16 part 1 solution

--- Advent Of Code 2020/Days/Day16.cs
+++ Advent Of Code 2020/Days/Day16.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace Advent_Of_Code_2020.Days
@@ -14,7 +16,29 @@ namespace Advent_Of_Code_2020.Days
 
         public override string SolvePart1()
         {
-            return "";
+            var input = File.ReadAllLines(_inputPath).ToList();
+
+            var validValues = new HashSet<int>();
+            for (int i = 0; i < 19; i++)
+            {
+                var instance = input[i].Split(new string[] { " ", "-" }, StringSplitOptions.RemoveEmptyEntries).ToList().Where(x => int.TryParse(x, out int y)).Select(x => int.Parse(x)).ToList();
+
+                for (int x = 0; x < instance.Count(); x += 2)
+                    validValues.UnionWith(Enumerable.Range(instance[x], (instance[x + 1] - instance[x] + 1)));
+            }
+
+            var invalidValues = new List<int>();
+            for (int i = 25; i < input.Count(); i++)
+            {
+                var instance = input[i].Split(',').Select(x => int.Parse(x)).ToList();
+                foreach (var value in instance)
+                {
+                    if (!validValues.Contains(value))
+                        invalidValues.Add(value);
+                }
+            }
+
+            return invalidValues.Sum().ToString();
         }
 
         public override string SolvePart2()
