ceeee8cad1e4409e2066bb5a5be244cb0aa5222e
Alexander Kelman <alexanderkelman95@hotmail.com>
2020-12-18 19:46:31 +0100

    Added Day 17 & 18

---
+++ Advent Of Code 2020/Days/Day18.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advent_Of_Code_2020.Days
+{
+    public class Day18 : DayBase
+    {
+        public Day18(int day)
+            : base(day)
+        {
+
+        }
+
+        public override string SolvePart1()
+        {
+            return "";
+        }
+
+        public override string SolvePart2()
+        {
+            return "";
+        }
+    }
+}
--- Advent Of Code 2020/Program.cs
+++ Advent Of Code 2020/Program.cs
@@ -9,7 +9,7 @@ namespace Advent_Of_Code_2020
         {
             Console.WriteLine("Advent of Code 2020");
 
-            var day = new Day16(16);
+            var day = new Day18(18);
 
             var result = day.SolvePart1();
 
