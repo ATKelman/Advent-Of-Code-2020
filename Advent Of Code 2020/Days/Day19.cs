455916f4b4070238a22d95e82e94663b0987ebbd
Alexander Kelman <alexanderkelman95@hotmail.com>
2020-12-20 16:27:13 +0100

    Added Day 19 part 1 & 2 solutions

--- Advent Of Code 2020/Days/Day19.cs
+++ Advent Of Code 2020/Days/Day19.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Advent_Of_Code_2020.Days
@@ -14,12 +16,42 @@ namespace Advent_Of_Code_2020.Days
 
         public override string SolvePart1()
         {
-            return "";
+            var input = File.ReadAllText(_inputPath).Split("\r\n\r\n").ToList();
+            var rules = input[0].Split(Environment.NewLine).Select(x => x.Split(": ")).ToDictionary(y => int.Parse(y[0]), y => y[1]);
+
+            var instance = rules[0];
+            while (Regex.IsMatch(instance, "\\d+"))
+                instance = Regex.Replace(instance, "\\d+", m => "(" + rules[int.Parse(m.Value)] + ")");
+
+            var pattern = new Regex("^" + instance.Replace(" ", "").Replace("\"", "") + "$");
+            return input[1].Split(Environment.NewLine).Count(x => pattern.IsMatch(x)).ToString();
         }
 
         public override string SolvePart2()
         {
-            return "";
+            var input = File.ReadAllText(_inputPath).Split("\r\n\r\n").ToList();
+            var rules = input[0].Split(Environment.NewLine).Select(x => x.Split(": ")).ToDictionary(y => int.Parse(y[0]), y => y[1]);
+            rules[8] = "42 | 42 8";
+            rules[11] = "42 31 | 42 11 31";
+
+            while (true)
+            {
+                var rule = rules.FirstOrDefault(x => !Regex.IsMatch(x.Value, "\\d+") && x.Key != 42 && x.Key != 31);
+                if (rule.Equals(default(KeyValuePair<int, string>)))
+                    break;
+
+                foreach (var i in rules.Keys.ToArray())
+                {
+                    rules[i] = Regex.Replace(rules[i], "\\b" + rule.Key + "\\b", "(" + rule.Value + ")");
+                }
+                rules.Remove(rule.Key);
+            }
+
+            rules[31] = rules[31].Replace(" ", "").Replace("\"", "");
+            rules[42] = rules[42].Replace(" ", "").Replace("\"", "");
+
+            var pattern = new Regex("^" + rules[0].Replace("8", "(" + rules[42] + ")+").Replace("11", "(?<A>" + rules[42] + ")+(?<-A> " + rules[31] + ")+").Replace(" ", "") + "$");
+            return input[1].Split(Environment.NewLine).Count(x => pattern.IsMatch(x)).ToString();
         }
     }
 }
