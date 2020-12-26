28207e3c5e1848eae01e6a6f39d8430145c86b27
Alexander Kelman <alexanderkelman95@hotmail.com>
2020-12-26 15:52:03 +0100

    Added Day 22 solutions for part 1 & 2

--- Advent Of Code 2020/Days/Day22.cs
+++ Advent Of Code 2020/Days/Day22.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,12 +18,81 @@ namespace Advent_Of_Code_2020.Days
 
         public override string SolvePart1()
         {
-            return "";
+            var input = File.ReadAllText(_inputPath).Split("\r\n\r\n").Select(x => x.Split("\r\n").Skip(1).Select(x => int.Parse(x)).ToList()).ToArray();
+
+            var playerOne = new Queue<int>(input[0]);
+            var playerTwo = new Queue<int>(input[1]);
+
+            while (playerOne.Count > 0 && playerTwo.Count > 0)
+            {
+                var playerOnesNumber = playerOne.Dequeue();
+                var playerTwosNumber = playerTwo.Dequeue();
+
+                if (playerOnesNumber > playerTwosNumber)
+                {
+                    playerOne.Enqueue(playerOnesNumber);
+                    playerOne.Enqueue(playerTwosNumber);
+                }
+                else
+                {
+                    playerTwo.Enqueue(playerTwosNumber);
+                    playerTwo.Enqueue(playerOnesNumber);
+                }
+            }
+
+            var winner = (playerOne.Count > 0 ? playerOne : playerTwo);
+            return winner.ToArray().Select((x, i) => x * (winner.Count - i)).Sum(x => x).ToString();
         }
 
         public override string SolvePart2()
         {
-            return "";
+            var input = File.ReadAllText(_inputPath).Split("\r\n\r\n").Select(x => x.Split("\r\n").Skip(1).Select(x => int.Parse(x)).ToList()).ToArray();
+
+            var playerOne = new Queue<int>(input[0]);
+            var playerTwo = new Queue<int>(input[1]);
+
+            var winner = RecursiveCombat(playerOne, playerTwo);
+            return winner.winnerQueue.ToArray().Select((x, i) => x * (winner.winnerQueue.Count - i)).Sum(x => x).ToString();
+        }
+
+        private (bool playerOneWon, Queue<int> winnerQueue) RecursiveCombat(Queue<int> playerOne, Queue<int> playerTwo)
+        {
+            var seen = new HashSet<(string, string)>();
+
+            var gameAlive = true;
+            while (gameAlive)
+            {
+                var playerOnesNumber = playerOne.Dequeue();
+                var playerTwosNumber = playerTwo.Dequeue();
+
+                var CurrentDecks = (string.Join("", playerOne.ToList()), string.Join("", playerTwo.ToList()));
+
+                if (seen.Contains(CurrentDecks))
+                    return (true, playerOne);
+                seen.Add(CurrentDecks);
+
+                var playerOneWonRound = true;
+                if (playerOnesNumber <= playerOne.Count() && playerTwosNumber <= playerTwo.Count()) // Sub Game
+                    playerOneWonRound = RecursiveCombat(new Queue<int>(playerOne.Take(playerOnesNumber).ToList()), new Queue<int>(playerTwo.Take(playerTwosNumber).ToList())).playerOneWon;
+                else if (playerOnesNumber < playerTwosNumber) // Regular Game
+                    playerOneWonRound = false;
+
+                if (playerOneWonRound)
+                {
+                    playerOne.Enqueue(playerOnesNumber);
+                    playerOne.Enqueue(playerTwosNumber);
+                }
+                else
+                {
+                    playerTwo.Enqueue(playerTwosNumber);
+                    playerTwo.Enqueue(playerOnesNumber);
+                }
+
+                if (playerOne.Count == 0 || playerTwo.Count == 0)
+                    gameAlive = false;
+            }
+
+            return (playerOne.Count > 0 ? (true, playerOne) : (false, playerTwo));
         }
     }
 }
--- Advent Of Code 2020/Program.cs
+++ Advent Of Code 2020/Program.cs
@@ -9,7 +9,7 @@ namespace Advent_Of_Code_2020
         {
             Console.WriteLine("Advent of Code 2020");
 
-            var day = new Day21(21);
+            var day = new Day22(22);
 
             var result = day.SolvePart1();
 
